9484be63b26861e8b41da5df165c989c5581d2b4
Slav <slav.1994@icloud.com>
2023-11-08 11:37:59 +0200

    new

--- src/components/Screens/Home/InputForm/InputForm.jsx
+++ src/components/Screens/Home/InputForm/InputForm.jsx
@@ -1,30 +1,38 @@
-import {useRef} from 'react'
+import {useState, useRef} from 'react'
 import styles from './InputForm.module.css'
 
 function InputForm({onFormSubmit}) {
+  const [error, setError] = useState(false)
+
   const inputRef = useRef();
 
-const getValue = (e) =>{
-  e.preventDefault();
-  const inputValue = inputRef.current.value;
-  if(inputValue){
-    onFormSubmit(inputValue)
-    inputRef.current.value = '';
-  }else{
-    return alert("Please enter your todo")
+  const getValue = (e) => {
+    e.preventDefault();
+    const inputValue = inputRef.current.value;
+    if (inputValue) {
+      onFormSubmit(inputValue)
+      inputRef.current.value = '';
+      setError(false)
+    } else {
+      setError(true)
+    }
   }
-   
-}
+
+  const handleInput =()=>{
+    if(error){
+      setError(false)
+    }
+  }
 
   return (
     <form onSubmit={getValue} className={styles.form__wrapper}>
       <input
         ref={inputRef}
-        className={styles.input}
+        className={`${styles.input} ${error ? styles.error : ''}`}
         type="text"
         placeholder="Enter your todo"
         defaultValue=""
-      />
+        onInput={handleInput}/>
       <button className={styles.button}>Add</button>
     </form>
   )
